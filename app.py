326f2ebd03d7a0365bb5547b364510421c18abad
Makshoof Kamal Jilani <140198597+makshoof@users.noreply.github.com>
2025-05-31 14:30:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ elif campus == "NED University":
 course = st.selectbox("Select Course 📚", courses)
 if course:
     st.success(f"You selected: {option} > {campus if campus else 'Online'} > {course}")
-    contact_number = "📞 Contact: +92 317 1719466--+92 316 1258459 if option == "Online" else "📞 Contact: +92 317 1719466--+92 316 1258459"
+    contact_number = "📞 Contact: +92 317 1719466 if option == "Online" else "📞 Contact: +92 316 1258459"
     st.info(contact_number)
 
 # Step 3: User Questions & Answers
